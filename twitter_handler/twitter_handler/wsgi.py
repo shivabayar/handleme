064c1bd1662541284a8beb53ad08d94c89ff9fc2
Shivraj S <shivrajs.bayar@gmail.com>
2016-04-10 18:36:37 +0530

    changing proc

--- twitter_handler/twitter_handler/wsgi.py
+++ twitter_handler/twitter_handler/wsgi.py
@@ -1,8 +1,7 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitter_handler.settings")
 
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
