9f513bf0fda10a043eed436be1085382a8ba6432
Shivraj S <shivrajs.bayar@gmail.com>
2016-04-10 18:06:37 +0530

    changin wsgi.py

--- twitter_handler/twitter_handler/wsgi.py
+++ twitter_handler/twitter_handler/wsgi.py
@@ -1,3 +1,6 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitter_handler.settings")
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
