4d3fa646e8f4cd7978e706da1125d4136e117708
Shivraj S <shivrajs.bayar@gmail.com>
2016-04-10 17:26:25 +0530

    adding sqllite

--- twitter_handler/twitter_handler/settings.py
+++ twitter_handler/twitter_handler/settings.py
@@ -30,14 +30,11 @@ OUT_DIR = os.path.join(BASE_DIR, "out")
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'handleme',
-        'USER': 'root',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'handleme'
     }
 }
+
 
 DEBUG = True
 
