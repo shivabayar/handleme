d799c50c39cf84644b349a10907fc9556d949feb
Shivraj S <shivrajs.bayar@gmail.com>
2016-04-10 15:55:07 +0530

    timeline page half complete

--- twitter_handler/twitter_handler/web/urls.py
+++ twitter_handler/twitter_handler/web/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 import views
+
 
 urlpatterns = [
     url(r'^$', views.get_index_page),
+    url(r'^search/twitter/query/(?P<handler>([aA-zZ]+))/$', views.UserViewSet.as_view()),
 ]
--- twitter_handler/twitter_handler/web/views.py
+++ twitter_handler/twitter_handler/web/views.py
@@ -1,20 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 import twitter
+from rest_framework.views import APIView
+from setuptools.command import register
 
-
-def get_index_page(request):
-    api = twitter.Api(consumer_key='YM7lZLIwUlaKh70kcMFuodMQl',
+api = twitter.Api(consumer_key='YM7lZLIwUlaKh70kcMFuodMQl',
                       consumer_secret='bsFtG0RVgaSlYkVUy3at99JuIdOdtsfRpVWf4SBbxII0yK0zyK',
                       access_token_key='391498643-YrBYllBegsfTSPYslZHLpg86FiAsiFLTfd2HLDF7',
                       access_token_secret='LgAQ8qP3XFRJX4vfauxKr0SB1QHm3ZonKyZSoBuFLI79R')
 
-    timeline = api.GetUserTimeline(screen_name='iamsrk', exclude_replies=False, include_rts=True)
-    # followers = api.GetFollowers(screen_name='shiva_bayar', count=10, include_user_entities=False)
 
-    for time in timeline:
-        print time.text
+def get_index_page(request):
+
+    # print "len:" + str(len(timeline))
+    # tweets = api.GetFavorites(user_id='2597413135', screen_name='RashtrapatiBhvn', include_entities=True, count=1000,
+    #                           since_id=1)
+    # print len(tweets)
+    # for tweet in tweets:
+    #     print tweet.text
+    #     print '--------'
+
+    # for time in timeline:
+    #     print time.text
+    #     print '--------'
+        # print time.id
 
     # for follower in followers:
     #     print follower
 
-    return render(request, 'index.html')
+    return render(request, 'search-box.html')
+
+
+class UserViewSet(APIView):
+
+
+    def get(self, request, handler):
+        timeline = api.GetUserTimeline(screen_name=handler, exclude_replies=False,
+                               include_rts=True, count=10)
+
+
+        # if len(timeline) > 0:
+        profile_pic = str(timeline[0].user.profile_image_url)
+        profile_pic = profile_pic.replace("normal", "200x200")
+        name = timeline[0].user.name
+        profile_url = timeline[0].user.url
+        banner_url = timeline[0].user.profile_banner_url
+
+        import pdb
+        pdb.set_trace()
+
+        response_dict = {'handler': handler,
+                         'profile_pic': profile_pic,
+                         'name': name,
+                         'profile_url': profile_url,
+                         'banner_url': banner_url,
+                         'timeline': timeline,
+                         'timeline_len': len(timeline)}
+        return render(request, 'search-result.html', response_dict)
