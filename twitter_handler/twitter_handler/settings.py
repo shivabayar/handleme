767fbc00d02bfc1b27cb8c8b31ffd3714cb28c7e
Shivraj S <shivrajs.bayar@gmail.com>
2016-04-10 18:22:40 +0530

    removing db

--- twitter_handler/twitter_handler/settings.py
+++ twitter_handler/twitter_handler/settings.py
@@ -28,8 +28,8 @@ OUT_DIR = os.path.join(BASE_DIR, "out")
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-import dj_database_url
-db_from_env = dj_database_url.config()
+# import dj_database_url
+# db_from_env = dj_database_url.config()
 
 DATABASES = {
     'default': {
@@ -39,8 +39,8 @@ DATABASES = {
 }
 
 # Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
